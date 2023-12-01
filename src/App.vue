1b779d74412ae0ddeb7122f7eb53f73bac5501be
Dan11yal <220107072@stu.sdu.edu.kz>
2023-12-01 23:44:39 +0600

    third part

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,59 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="main">
+  <carousel :itemsToShow="1.5">
+    <slide v-for="(list, index) in lists" :key="index">
+      <img :src="require(`./assets/${list.photo}`)" alt="Slide" />
+    </slide>
+
+    <template #addons>
+      <navigation />
+      <pagination />
+    </template>
+  </carousel>
+</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import 'vue3-carousel/dist/carousel.css';
+import { Carousel, Slide, Pagination, Navigation } from 'vue3-carousel';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
-}
+    Carousel,
+    Slide,
+    Pagination,
+    Navigation,
+  },
+  data() {
+    return {
+      lists: [
+        { photo: '1.jpg' },
+        { photo: '2.jpg' },
+        { photo: '3.jpg' },
+        { photo: '4.jpg' },
+        { photo: '5.jpg' },
+      ],
+    };
+  },
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+*{
+  margin: 0;
+  padding: 0;
+}
+.main{
+ padding:120px 100px;
+ background-color: burlywood;
+}
+
+img {
+  width: 500px;
+  height: 500px;
+  border-radius:10%;
+  outline: none;
+  border: none;
 }
 </style>
